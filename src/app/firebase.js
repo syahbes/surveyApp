ebf9e82bf4c1118553a0049c280eda2ad926ed10
Shlomi Yahbes <99732661+syahbes@users.noreply.github.com>
2023-05-07 23:46:14 +0300

    Update firebase.js

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8I5RvvSATFJuS7z97NDaqGnEY6noFwsc",
-  authDomain: "surveyapp-c5aff.firebaseapp.com",
-  projectId: "surveyapp-c5aff",
-  storageBucket: "surveyapp-c5aff.appspot.com",
-  messagingSenderId: "72951752720",
-  appId: "1:72951752720:web:170554041385471c61c295"
+  
 };
 
 // if (!firebase.apps.length) {
@@ -14,24 +9,3 @@ firebase.initializeApp(firebaseConfig);
 // }
 
 export { firebase };
-
-
-
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// import { getAuth, signOut } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD8I5RvvSATFJuS7z97NDaqGnEY6noFwsc",
-//   authDomain: "surveyapp-c5aff.firebaseapp.com",
-//   projectId: "surveyapp-c5aff",
-//   storageBucket: "surveyapp-c5aff.appspot.com",
-//   messagingSenderId: "72951752720",
-//   appId: "1:72951752720:web:170554041385471c61c295"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const db = getFirestore(app);
-
-// export const auth = getAuth(app);
-// export const fbSignOut = async () => await signOut(auth);
